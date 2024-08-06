35331e6c3f3a7c472e65d046cf37e1782e12ab66
AshwekPadolkar <131592430+ashwek-padolkar@users.noreply.github.com>
2024-08-07 00:35:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors(
 ));
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://padolkarashwek11:sachintendulkar10@cluster0.i9s5ver.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 
 app.get("/", (req, res) => {
